4fcceead2ded1ba445bfffa18f08cc5664b67bc2
Michele Locati <michele@locati.it>
2017-06-14 08:31:20 +0200

    Fix TextDecoder/TextEncoder option names

--- text-encoding.d.ts
+++ text-encoding.d.ts
@@ -4,14 +4,14 @@
 
 declare class TextEncoder 
 {
-    constructor(label?: string, options?: TextEncoding.TextDecoderOptions);
+    constructor(label?: string, options?: TextEncoding.TextEncoderOptions);
     encoding: string;
     encode(input?: string, options?: TextEncoding.TextEncodeOptions): Uint8Array;
 }
 
 declare class TextDecoder
 {
-    constructor(utfLabel?: string, options?: TextEncoding.TextEncoderOptions)
+    constructor(utfLabel?: string, options?: TextEncoding.TextDecoderOptions)
     encoding: string;
     fatal: boolean;
     ignoreBOM: boolean;
